9cc01bc94214858f68527fe92addb122ade1a336
Canaan Seaton <cmseaton42@gmail.com>
2017-04-22 15:28:08 -0500

    response to slack working

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const querystring = require('querystring');
 const spotify = require('./spotify');
 
 const app = express();
+
+let queue = [];
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -75,7 +78,8 @@ app.post('/jukebox', (req, res) => {
                 for (let item of searchData.tracks.items) {
                     let track = {};
 
-                    track.color = "#269900";
+                    track.color = "#03c2fb";
+                    track.callback_id = "Track_Search";
                     track.thumb_url = item.album.images[1].url;
                     track.title = item.name;
 
@@ -94,10 +98,16 @@ app.post('/jukebox', (req, res) => {
 
                     track.actions = [
                         {
-                            "name": "search",
+                            "name": "AddToQueue",
                             "text": "Add to Queue",
                             "type": "button",
-                            "value": item.uri
+                            "value": JSON.stringify({
+                                album_name: item.album.name,
+                                album_url: item.album.images[1].url,
+                                artist: item.artists[0].name,
+                                track_uri: item.uri,
+                                track_name: item.name
+                            })
                         }
                     ]
 
@@ -119,12 +129,60 @@ app.post('/jukebox', (req, res) => {
             }
 
         });
-
-
     }
 })
 
+app.post('/interactions', (req, res) => {
+    let payload = JSON.parse(req.body.payload);
 
+    if (payload.token === process.env.SLACK_TOKEN) {
+        let actions = payload.actions;
+
+        switch (payload.callback_id) {
+            case 'Track_Search':
+                let track = payload.actions[0].value;
+                track = JSON.parse(track);
+
+                let user = payload.user;
+
+                queue.push(track);
+
+                console.log(chalk.green(queue.length));
+
+                res.status(200).json({
+                    text: ":heavy_check_mark: @" + user.name + " added _*" + track.track_name + "*_ to the play queue... :notes::musical_note:",
+                    mrkdwn: true,
+                    replace_url: true,
+                    response_type: "in_channel",
+                    attachments: [
+                        {
+                            color: "#01d509",
+                            thumb_url: track.album_url,
+                            fields: [
+                                {
+                                    title: "Album",
+                                    value: track.album_name,
+                                    short: true
+                                },
+                                {
+                                    title: "Artist",
+                                    value: track.artist,
+                                    short: true
+                                }
+                            ]
+                        }
+                    ]
+                });
+                break;
+        
+            default:
+                break;
+        }
+
+    } else {
+        res.status(403).end();
+    }
+});
 
 app.listen(process.env.PORT, () => {
     console.log("Listening on Port: " + process.env.PORT + "...");
